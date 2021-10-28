2609cd1f02d77d15eb977e706ac6c5032776e4dc
domtronn <dgc336@gmail.com>
2021-10-28 22:44:22 +0100

    Add recommendations for party composition to each gym

--- src/lib/components/gym-card.svelte
+++ src/lib/components/gym-card.svelte
@@ -5,6 +5,30 @@
   const teamCap = pokemon.length
   const maxStat = pokemon.reduce((acc, it) => Math.max(acc, Math.max(...Object.values(it.stats))), 0)
 
+  const threshold = -15
+
+  const atkMoves = pokemon.reduce((acc, it) => acc + it.moves.filter(i => i.damage_class !== 'status').length, 0)
+  const physMoves = pokemon.reduce((acc, it) => acc + it.moves.filter(i => i.damage_class === 'physical').length, 0)
+  const specMoves = pokemon.reduce((acc, it) => acc + it.moves.filter(i => i.damage_class === 'special').length, 0)
+
+  const aggStats = pokemon.reduce((acc, it, i, arr) => ({
+    spdef: (acc.spdef || []).concat(it.stats.spdef),
+    def: (acc.def || []).concat(it.stats.def),
+    spatk: (acc.spatk || []).concat(it.stats.spatk),
+    atk: (acc.atk || []).concat(it.stats.atk),
+  }), {})
+
+  const stats = Object
+    .entries(aggStats)
+    .map(([key, vals]) => [
+      key, {
+        min: Math.min(...vals),
+        max: Math.max(...vals),
+        avg: vals.reduce((acc, it) => acc + it, 0) / vals.length
+      }
+    ])
+    .reduce((acc, [key, val]) => ({ ...acc, [key]: val }), {})
+
   import Pokemon from '$lib/components/pokemon-card.svelte'
   import TypeBadge from '$lib/components/type-badge.svelte'
   import Label from '$lib/components/label.svelte'
@@ -29,6 +53,29 @@
       {#if speciality}
         <h2 class='text-md font-medium'>The {speciality} gym</h2>
       {/if}
+
+{#if stats.def.avg - stats.spdef.avg <= threshold}
+  <p>Recommend using a <b>Physical attacker</b> since their <code>def</code> is a weakness.</p>
+{:else if stats.spdef.avg - stats.def.avg <= threshold}
+  <p>Recommend using a <b>Special attacker</b> since their <code>spdef</code> is a weakness.</p>
+{:else}
+  <p>Recommend using a <b>mixture</b> as their defenses are balanced.</p>
+{/if}
+
+{#if stats.atk.avg - stats.spatk.avg <= threshold}
+  <p>Their team is strong <b>Special attackers</b>.</p>
+{:else if stats.spatk.avg - stats.atk.avg <= threshold}
+  <p>Their team is strong <b>Physical attackers</b>.</p>
+{:else}
+  <p>Their team uses <b>mixed</b> attackers.</p>
+{/if}
+
+{#if (physMoves / atkMoves) > 0.5}
+  <p>With most of their attacks being <b>physical</b> ({Math.round(physMoves / atkMoves * 100)}%).</p>
+{:else if (specMoves / atkMoves) > 0.5}
+  <p>With most of their attacks being <b>special</b> ({Math.round(specMoves / atkMoves * 100)}%).</p>
+{/if}
+
     </span>
 
   <div slot='item' class='grid lg:grid-cols-3 md:grid-cols-2 mt-8 gap-x-6 gap-y-10'>
--- src/lib/components/stat-block.svelte
+++ src/lib/components/stat-block.svelte
@@ -7,9 +7,11 @@
 
 </script>
 
-<div class='grid grid-cols-7 gap-x-2 gap-y-1 items-center'>
+<div class='grid grid-cols-7 gap-x-2 gap-y-1 items-center -mt-10'>
   <span class='col-span-3' />
-  <span class='col-span-4 -mb-1'><b>{total}</b></span>
+  <span class='col-span-4 mb-1'>
+    <b class='bg-white py-1 -ml-2 pl-2 pr-2.5 rounded-lg'>{total}</b>
+  </span>
 
   {#each Object.entries(stats) as [s, sval]}
     <StatBar stat={s} val={sval} max={max} />
