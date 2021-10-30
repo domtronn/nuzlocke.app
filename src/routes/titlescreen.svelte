bea095a6094838404d86212a1eaa8de77ab25142
domtronn <dgc336@gmail.com>
2021-10-30 15:15:16 +0100

    Add hover effects

--- src/routes/titlescreen.svelte
+++ src/routes/titlescreen.svelte
@@ -14,6 +14,9 @@
   const interval = 5000
   const distance = 300
 
+  let hovering = false
+  const toggleHover = _ => hovering = !hovering
+
   setInterval(() => {
     flip = (flip + 1) % 2
     id = Math.round(Math.random() * 898)
@@ -26,25 +29,42 @@
 <main class='container mx-auto h-screen flex flex-col justify-center'>
 
   <span class='mx-auto text-center'>
-    <img src='/pkmn-logo.png' class='grayscale h-32 -translate-x-2 translate-y-2 mx-auto'/>
+    <img class:grayscale={!hovering} src='/pkmn-logo.png' class='transition h-32 -translate-x-2 translate-y-2 mx-auto'/>
     <h1 class='font-mono text-5xl'>Nuzlocke tracker</h1>
   </span>
 
   <div class='py-11 px-10 sm:px-4 overflow-hidden mt-10'>
-    <PixelatedContainer className='font-mono relative h-32 max-w-md mx-auto tracking-widest flex sm:flex-row justify-evenly items-center gap-y-2 text-3xl py-2 w-auto'>
+    <PixelatedContainer className='font-mono relative h-32 max-w-md mx-auto flex sm:flex-row justify-evenly items-center gap-y-2 text-3xl py-2 w-auto'>
 
       <div class='font-bold flex flex-col'>
-        <span>New Game</span>
-        <span>Load Game</span>
-        <span>Settings</span>
+
+        <span>
+          <button class='tracking-widest hover:drop-shadow-text hover:text-yellow-300 transition duration-300' on:mouseenter={toggleHover} on:mouseleave={toggleHover}>
+            New Game
+          </button>
+        </span>
+
+        <span>
+          <button class='tracking-widest hover:drop-shadow-text hover:text-blue-400 transition duration-300' on:mouseenter={toggleHover} on:mouseleave={toggleHover}>
+            Load Game
+          </button>
+        </span>
+
+
+        <span>
+          <button class='tracking-widest hover:drop-shadow-text hover:text-orange-400 transition duration-300' on:mouseenter={toggleHover} on:mouseleave={toggleHover}>
+            Settings
+          </button>
+        </span>
+
       </div>
 
       <div class='img__container h-full -mx-12 relative'>
         {#if !flip}
-          <img src={src} out:fly={{ y: distance, duration }} in:fly={{ y: -distance, duration }} class='absolute right-0 grayscale w-full -my-2 md:-my-12 -ml-12' />
+          <img class:grayscale={!hovering} src={src} out:fly={{ y: distance, duration }} in:fly={{ y: -distance, duration }} class='absolute transition right-0 w-full -my-2 md:-my-12 -ml-12' />
         {/if}
         {#if flip}
-          <img src={src} out:fly={{ y: distance, duration }} in:fly={{ y: -distance, duration }} class='absolute right-0 grayscale w-full -my-2 md:-my-12 -ml-12' />
+          <img class:grayscale={!hovering} src={src} out:fly={{ y: distance, duration }} in:fly={{ y: -distance, duration }} class='absolute transition right-0 grayscale w-full -my-2 md:-my-12 -ml-12' />
         {/if}
       </div>
     </PixelatedContainer>
@@ -65,5 +85,6 @@
 
   img {
     image-rendering: pixelated;
+    transition-duration: 300ms !important;
   }
 </style>
