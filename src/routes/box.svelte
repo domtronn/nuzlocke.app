0b644fce75332ee161108512f570301c0ddb1188
domtronn <dgc336@gmail.com>
2021-11-03 12:04:13 +0000

    Responsive box work

--- src/routes/box.svelte
+++ src/routes/box.svelte
@@ -49,53 +49,56 @@
 
 <div transition:fade class='container mx-auto'>
   <div class='flex flex-col mx-auto items-center justify-center'>
-    <main role='main' class='w-full sm:w-2/3 md:w-3/4 grid grid-cols-3 md:grid-cols-8 gap-x-4 gap-y-4 md:gap-y-8 py-6 px-4 md:px-8 overflow-hidden'>
+    <main role='main' class='w-full sm:w-2/3 md:w-3/4 flex flex-col gap-y-4 py-6 px-4 md:px-8 overflow-hidden'>
+
       <GameHeading className='col-span-8' />
-      <div class='grid grid-cols-5 gap-x-2 gap-y-2 col-span-3'>
-        {#each types as t}
-          {#if typeCounts[t] > 0}
+
+      <div class='inline-flex flex-wrap flex-col sm:flex-row gap-y-2 gap-x-4 sm:items-start'>
+        <div class='grid grid-cols-6 w-full sm:w-auto sm:grid-cols-3 gap-2 sm:gap-2 col-span-2'>
+          {#each stats as s}
             <label
-              class='transition cursor-pointer'
-              class:grayscale={type && type !== t}
-              class:opacity-50={type && type !== t}
-              class:grayscale-0={type && type === t}
-            >
-              <input type=radio bind:group={type} name='filter' value={t} />
-              <TypeBadge type={t} className='w-full justify-center' />
+              class='transition cursor-pointer inline-flex text-center text-xs px-2 py-1 w-full text-gray-400 border-gray-300 font-medium border shadow-sm rounded-lg inline-flex'
+              class:border-black={stat === s}
+              class:text-black={stat === s}
+              >
+              <input type=radio bind:group={stat} name='sortable' value={s} />
+              <Icon className='text-xxs {s !== 'spa' ? 'fill-current' : ''} translate-y-1/2 -mt-0.5 mr-1'  src={StatIconMap[s]} />
+              {s}
             </label>
-          {/if}
-        {/each}
+          {/each}
+        </div>
+
+        <div class='grid grid-cols-5 sm:grid-cols-9 gap-x-2 gap-y-2 col-span-3'>
+          {#each types as t}
+            {#if typeCounts[t] > 0}
+              <label
+                class='transition cursor-pointer'
+                class:grayscale={(type && type !== t) || !typeCounts[t]}
+                class:opacity-50={(type && type !== t) || !typeCounts[t]}
+                class:grayscale-0={type && type === t}
+                >
+                <input disabled={!typeCounts[t]} type=radio bind:group={type} name='filter' value={t} />
+                <TypeBadge type={t} className='w-full justify-center' />
+              </label>
+            {/if}
+          {/each}
+        </div>
+
+        <div class='sm:order-none order-first col-span-1 flex justify-end -mt-8 sm:mt-0'>
+          <button on:click={clear} class='inline-flex items-center' >
+            <Icon src={X} size='1.6em' />
+            Clear filters
+          </button>
+        </div>
       </div>
 
-      <div class='grid grid-cols-3 gap-2 col-span-2'>
-        {#each stats as s}
-          <label
-            class='transition cursor-pointer inline-flex text-center text-xs px-2 py-1 w-full text-gray-400 border-gray-300 font-medium border shadow-sm rounded-lg inline-flex'
-            class:border-black={stat === s}
-            class:text-black={stat === s}
+      <div class='grid grid-cols-2 md:grid-cols-4 gap-x-4'>
+        {#each box.filter(filter) as p (p.id)}
+          <span
+            animate:flip={{ duration: d => 10 * Math.sqrt(d) }}
+            out:fade={{ duration: 150 }}
+            class='my-3'
             >
-            <input type=radio bind:group={stat} name='sortable' value={s} />
-            <Icon className='text-xxs {s !== 'spa' ? 'fill-current' : ''} translate-y-1/2 -mt-0.5 mr-1'  src={StatIconMap[s]} />
-            {s}
-          </label>
-        {/each}
-      </div>
-
-      <div class='col-span-1 flex flex-col items-center justify-center'>
-        <button on:click={clear} class='inline-flex items-center' >
-          <Icon src={X} size='1.6em' />
-          Clear filters
-        </button>
-      </div>
-
-      <div class='col-span-2' />
-
-      {#each box.filter(filter) as p (p.id)}
-        <span
-          animate:flip={{ duration: d => 10 * Math.sqrt(d) }}
-          out:fade={{ duration: 150 }}
-          class='col-span-4 md:col-span-2 my-3'
-          >
             <PokemonCard
               sprite={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Pokemon[p.pokemon].num}.png`}
               maxStat={Math.max(150, ...Object.values(Pokemon[p.pokemon].baseStats))}
@@ -105,7 +108,8 @@
               types={(Pokemon[p.pokemon].types || []).map(t => t.toLowerCase())}
               />
           </span>
-      {/each}
+        {/each}
+      </div>
     </main>
   </div>
 </div>
