b8898e691038017bfdfd272b79b53707b8cd9d05
domtronn <dgc336@gmail.com>
2021-11-03 12:07:57 +0000

    Inline google font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,28 +3,36 @@
 </script>
 
 <style global lang="postcss">
-    .pkm-wrapper {
-      width: 67px;
-      height: 56px;
-      line-height: 54px;
-      text-align: center;
-      display: inline-block;
-      vertical-align: middle;
-      padding: 1px;
-    }
+  /* latin */
+  @font-face {
+    font-family: 'VT323';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url(https://fonts.gstatic.com/s/vt323/v12/pxiKyp0ihIEF2isfFJXUdVNF.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  }
 
-    .pkm {
-      display: inline-block;
-      vertical-align: middle;
-      image-rendering: crisp-edges;
-      image-rendering: pixelated;
-    }
+  .pkm-wrapper {
+    width: 67px;
+    height: 56px;
+    line-height: 54px;
+    text-align: center;
+    display: inline-block;
+    vertical-align: middle;
+    padding: 1px;
+  }
+
+  .pkm {
+    display: inline-block;
+    vertical-align: middle;
+    image-rendering: crisp-edges;
+    image-rendering: pixelated;
+  }
 </style>
 
 <svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
 </svelte:head>
 
 <slot />
