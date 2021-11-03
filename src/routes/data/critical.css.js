9c6db1db82c5006440ac197a80c8047c6f209b24
domtronn <dgc336@gmail.com>
2021-11-03 09:07:31 +0000

    Reference images for vite to import properly

--- src/routes/data/critical.css.js
+++ src/routes/data/critical.css.js
@@ -1,8 +1,6 @@
 import spritesheet from 'pokemon-assets/assets/css/pokemon.css'
 
 export async function get ({ params }) {
-  console.log(spritesheet)
-
   return {
     status: 200,
     body: spritesheet,
