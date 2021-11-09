280b8460de64f410bf8cbb9e672fc520553bc318
domtronn <dgc336@gmail.com>
2021-11-09 09:23:47 +0000

    Fix the level cap label for mobile

--- src/lib/components/label.svelte
+++ src/lib/components/label.svelte
@@ -2,7 +2,7 @@
   export let heading, body
 </script>
 
-<div class='h-10 w-10 md:h-14 md:w-14 flex flex-col text-center justify-center text-gray-500 px-2 py-1 border-2 dark:border-gray-700 border-gray-300 shadow-md rounded-lg'>
-  <span class='text-xxs leading-3 font-bold -mb-1'>{heading}</span>
-  <span class='font-bold text-lg md:text-2xl'>{body}</span>
+<div class='h-12 w-14 md:h-14 md:w-14 flex flex-col text-center justify-center text-gray-500 p-1 md:border-2 dark:border-gray-700 border-gray-300 md:shadow-md rounded-lg'>
+  <span class='md:scale-100 text-tiny leading-3'>{heading}</span>
+  <span class='font-bold text-2xl'>{body}</span>
 </div>
