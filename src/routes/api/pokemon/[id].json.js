8af5ffbb887a439c7fd78a91234e91583288a215
domtronn <dgc336@gmail.com>
2021-11-13 08:05:18 +0000

    [fix] Fix evos for names that don't match key

--- src/routes/api/pokemon/[id].json.js
+++ src/routes/api/pokemon/[id].json.js
@@ -1,16 +1,14 @@
 import Pokemon from 'pokemon-assets/assets/data/pokemon.json'
-const PokemonNum = Object
-  .values(Pokemon)
-  .reduce((acc, it) => ({
-    ...acc,
-    [it.num]: it
-  }), {})
+const rotate = (keyF, o) => Object.values(o).reduce((acc, it) => ({ ...acc, [keyF(it)]: it }), {})
+
+const PokemonNum = rotate(it => it.name, Pokemon)
+const PokemonName = rotate(it => it.name.toLowerCase(), Pokemon)
 
 // Need id,name,alias,sprite,evos
 
 export async function get ({ params }) {
   const { id } = params
-  const pkmn = Pokemon[id] || PokemonNum[id]
+  const pkmn = Pokemon[id] || PokemonNum[id] || PokemonName[id]
 
   if (!pkmn)
     return { status: 404 }
