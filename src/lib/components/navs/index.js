68572ebb7fe21aab84a258e633d6bde6de4233f2
domtronn <dgc336@gmail.com>
2021-11-17 07:01:31 +0000

    [refactor] Move Cookie Banner under navs

--- src/lib/components/navs/index.js
+++ src/lib/components/navs/index.js
@@ -1,2 +1,3 @@
 export { default as NavHeading } from './NavHeading.svelte'
 export { default as GameHeading } from './GameHeading.svelte'
+export { default as CookieBanner } from './CookieBanner.svelte'
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,10 +9,8 @@
 
   import '../app.css'
   import { onMount } from 'svelte'
-  import { slide } from 'svelte/transition'
 
-  import { GameHeading, NavHeading } from '$lib/components/navs'
-  import CookieBanner from '$lib/components/cookie-banner.svelte'
+  import { GameHeading, NavHeading, CookieBanner } from '$lib/components/navs'
 
   import Icon from 'svelte-icons-pack'
   import GitHub from 'svelte-icons-pack/bi/BiLogoGithub'
@@ -27,23 +25,6 @@
     font-display: swap;
     src: url(https://fonts.gstatic.com/s/vt323/v12/pxiKyp0ihIEF2isfFJXUdVNF.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-  }
-
-  .pkm-wrapper {
-    width: 67px;
-    height: 56px;
-    line-height: 54px;
-    text-align: center;
-    display: inline-block;
-    vertical-align: middle;
-    padding: 1px;
-  }
-
-  .pkm {
-    display: inline-block;
-    vertical-align: middle;
-    image-rendering: crisp-edges;
-    image-rendering: pixelated;
   }
 </style>
 
