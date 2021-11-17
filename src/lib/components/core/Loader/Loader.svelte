349a68c25943d311c0fffaeea844a2fadf25a778
domtronn <dgc336@gmail.com>
2021-11-17 07:04:03 +0000

    [fix] Improve height of loading spinner

--- src/lib/components/core/Loader/Loader.svelte
+++ src/lib/components/core/Loader/Loader.svelte
@@ -2,10 +2,15 @@
   import LoaderPip from './LoaderPip.svelte'
 </script>
 
-<div class='h-screen flex items-center justify-center transform rotate-12 scale-150'>
-  <div class='absolute flex items-center justify-center'>
+<div class='flex items-center justify-center transform rotate-12 scale-150'>
+  <div class='absolute flex items-center justify-center -translate-y-full top-1/2'>
     {#each Array(8).fill() as _, i}
       <LoaderPip delay={0.05 * i} />
     {/each}
   </div>
 </div>
+
+<style>
+  div { height: calc(100vh - 10rem); }
+  div > div { height: auto; }
+</style>
