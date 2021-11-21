6ce5ba6d7d1cb8e569ad450cc5d2e8116a631076
domtronn <dgc336@gmail.com>
2021-11-21 18:36:06 +0000

    [fix] Fix theme toggle text colors on mobile

--- src/lib/components/theme-toggle.svelte
+++ src/lib/components/theme-toggle.svelte
@@ -33,20 +33,20 @@
     }
   }
 
-  const duration = 300
+  const duration = 150
   const delay = duration
   const y = 50
 
 </script>
 
-<button class='{className} relative w-16 h-full umami--click--toggle-{$theme}' aria-label={$theme === 'dark' ? 'Light mode' : 'Dark mode'} on:click={toggleTheme}>
+<button class='{className} relative w-10 sm:w-16 h-full umami--click--toggle-{$theme}' aria-label={$theme === 'dark' ? 'Light mode' : 'Dark mode'} on:click={toggleTheme}>
   {#if $theme == 'light'}
     <div in:fadefly={{ duration, delay, y: -y }} out:fadefly={{ duration, y: -y }} >
-      <Icon size='1.2em' className='-mt-2 absolute left-1/2 -translate-x-1/2 text-white sm:hover:text-black sm:text-gray-800 transition fill-current' src={DarkMode} />
+      <Icon size='1.2em' className='-mt-2 absolute left-1/2 -translate-x-1/2 transition fill-current' src={DarkMode} />
     </div>
   {:else if $theme == 'dark'}
     <div in:fadefly={{ duration, delay, y }} out:fadefly={{ duration, y }}>
-      <Icon size='1.2em' className='-mt-2 absolute left-1/2 -translate-x-1/2 hover:text-white text-gray-200 transition fill-current' src={LightMode} />
+      <Icon size='1.2em' className='-mt-2 absolute left-1/2 -translate-x-1/2 transition fill-current' src={LightMode} />
     </div>
   {/if}
 </button>
