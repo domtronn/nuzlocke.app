c6cbe52060857641272549a84d6ebc8a4d6a0c26
domtronn <dgc336@gmail.com>
2021-11-21 09:43:23 +0000

    [feature] Add sprite sheet for Gym badges

---
+++ src/routes/assets/badges.css.js
@@ -0,0 +1,14 @@
+import Clean from 'clean-css'
+import badges from './badges.css'
+
+const clean = new Clean()
+const minifiedBadges = clean.minify(badges).styles
+
+export async function get () {
+  return {
+    status: 200,
+    body: minifiedBadges,
+    'Cache-Control': 'public, max-age=31536000',
+    'Content-Type': 'text/css'
+  }
+}
