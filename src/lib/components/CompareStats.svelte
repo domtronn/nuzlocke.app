3055fbb845a19287fa8e8c6829421550ef3e4753
domtronn <dgc336@gmail.com>
2021-11-28 16:26:32 +0000

    [fix] missed commit

---
+++ src/lib/components/CompareStats.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let pokemon = [], side
+
+  import TypeBadge from '$lib/components/type-badge.svelte'
+  import StatBlock from '$lib/components/stat-block.svelte'
+  import ColorMap from '$lib/data/colors.json'
+
+  const [atk, def] = pokemon
+  const col = ColorMap[atk.types[0].toLowerCase()]
+</script>
+
+<div class=flex-1>
+  <span class='flex gap-x-2 mb-2 -ml-2 justify-{side === 'left' ? 'start' : 'end'} transform scale-75'>
+    {#each atk.types as type}
+      <TypeBadge type={type.toLowerCase()} />
+    {/each}
+  </span>
+
+  <StatBlock
+    {...atk.baseStats}
+    compare={def.baseStats}
+    max={250}
+    {col}
+    {side}
+  />
+</div>
