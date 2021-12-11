c92fa08227b221ff29e5b52e5a8d3137201b3aea
domtronn <dgc336@gmail.com>
2021-12-11 16:14:53 +0000

    [feature] Implement search functionality for easier navigation

--- src/lib/components/core/Input.svelte
+++ src/lib/components/core/Input.svelte
@@ -1,19 +1,33 @@
 <script>
-  export let name, value = null, color = '', className = '', placeholder = '', rounded = false
+  export let name, value = null, color = '', className = '', placeholder = '', icon, rounded = false
+  import Icon from 'svelte-icons-pack'
   name = name || placeholder
 </script>
 
-<label for={name}>{name}</label>
-<input
-  id={name}
-  {placeholder}
-  type=text
-  bind:value={value}
-  class:rounded-lg={rounded}
-  class='{color} {className}'
-/>
+<div>
+  <label for={name}>{name}</label>
+  <input
+    id={name}
+    {placeholder}
+    type=text
+    bind:value={value}
+    class:rounded-lg={rounded}
+    class:pl-8={!!icon}
+    class:pl-3={!icon}
+    class='{color} {className}'
+    autocomplete=off
+  />
+  {#if icon}
+    <span>
+      <Icon src={icon} className='fill-current absolute left-0 top-1/2 -translate-y-1/2 ml-3' />
+    </span>
+  {/if}
+  <slot />
+</div>
 
 <style>
+  div { position: relative; }
+
   :root {
     --inp-focus: theme('colors.black');
     --inp-focus-2: theme('colors.gray.500');
@@ -29,7 +43,7 @@
 
   input {
     /* @apply  sm:text-xs */
-    @apply text-xxs sm:text-xs w-full transition-colors border-2 ring-2 ring-transparent text-gray-800 placeholder-gray-400 shadow-sm focus:outline-none px-3 h-10;
+    @apply text-xxs sm:text-xs w-full transition-colors border-2 ring-2 ring-transparent text-gray-800 placeholder-gray-400 shadow-sm focus:outline-none pr-3 h-10;
     background-color: var(--input-bg);
     border-color: theme('colors.gray.200');
   }
@@ -38,6 +52,11 @@
     @apply text-gray-100 placeholder-gray-500;
     border-color: theme('colors.gray.600');
   }
+
+  :global(.dark) div { @apply text-gray-500 transition }
+  :global(.dark) input:hover + span, :global(.dark) input:focus + span { @apply text-gray-100 }
+  div { @apply text-gray-400 transition }
+  input:hover + span, input:focus + span { @apply text-gray-800 }
 
   input:hover { border-color: var(--inp-focus-2); }
   input:focus {
