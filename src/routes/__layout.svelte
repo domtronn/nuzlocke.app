4f5cb2eaa77cb4640a6ff415cb04e05191b17f10
domtronn <dgc336@gmail.com>
2021-12-12 12:14:47 +0000

    [feature] Add a link to buy me a coffee in the footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -14,8 +14,10 @@
   import { GameHeading, NavHeading, CookieBanner } from '$lib/components/navs'
 
   import Icon from 'svelte-icons-pack'
+  import Coffee from 'svelte-icons-pack/si/SiBuymeacoffee'
   import GitHub from 'svelte-icons-pack/bi/BiLogoGithub'
   import Discord from 'svelte-icons-pack/bi/BiLogoDiscord'
+  import Heart from 'svelte-icons-pack/ti/TiHeartFullOutline'
 
   let data
   const fetchData = async () => {
@@ -50,7 +52,7 @@
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 
-  footer { @apply text-center w-full absolute bottom-0 my-8 sm:my-10 px-4 text-tiny md:text-xs text-gray-500 dark:text-gray-400 leading-3; }
+  footer { @apply text-center w-full absolute bottom-0 my-4 sm:my-6 px-4 text-tiny md:text-xs text-gray-500 dark:text-gray-400 leading-3; }
   footer > p { @apply flex justify-center text-2xl gap-x-2; }
   footer a,
   footer button { @apply transition-colors dark:hover:text-white hover:text-black; }
@@ -78,6 +80,12 @@
 
 <footer>
   <p>
+    <a href="https://www.buymeacoffee.com/nuzlocketracker"
+       rel=noreferrer target=_blank>
+      <button aria-label='Support me' title='Supprt me'>
+        <Icon className='fill-current' src={Coffee} />
+      </button>
+    </a>
     <a href="https://github.com/domtronn/dc-nuzlocke/issues/new?assignees=&labels=&template=bug_report.md&title="
        rel=noreferrer target=_blank>
       <button aria-label='Report bugs' title='Report bugs'>
@@ -96,4 +104,6 @@
   All content & design © Pokémon Nuzlocke Tracker, 2021-{(new Date()).getFullYear()}. <a href='/privacy-policy'>Privacy Policy</a>.
   <br class='hidden md:block' />
   Pokémon images & names © 1995-2021 Nintendo/Creatures Inc./GAME FREAK inc. TM
+  <br class='hidden md:block' />
+  <span class='inline-flex items-center mt-2'>Made with <Icon src={Heart} className='mx-1 fill-current text-red-500' /> by<strong class=ml-1>Domtronn</strong></span>
 </footer>
