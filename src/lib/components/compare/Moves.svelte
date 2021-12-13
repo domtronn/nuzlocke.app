79130a59f35923707002abc8f9f598ef49f27a0a
domtronn <dgc336@gmail.com>
2021-12-13 17:27:08 +0000

    [fix] Add missing component

---
+++ src/lib/components/compare/Moves.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let calc, pokemon = []
+  const [team, opp] = pokemon
+
+  import TypeBadge from '$lib/components/type-badge.svelte'
+
+</script>
+
+<span class='text-sm -mb-1 md:hidden'>
+  {opp.name}'s moveset
+</span>
+
+<ul class='grid grid-cols-2 w-full md:mt-2 text-sm md:text-xs'>
+  {#each calc[team.sprite][opp.sprite] as move}
+    <li class=my-2>
+      <p class=mb-1>
+        <strong>
+          {move.name}
+        </strong>
+
+        <span class='text-gray-500'>({move.dmgrange})</span>
+      </p>
+
+      <p class=inline-flex>
+        <TypeBadge type={move.damage_class} />
+        <TypeBadge type={move.type} className='ml-1' />
+      </p>
+
+    </li>
+  {/each}
+</ul>
