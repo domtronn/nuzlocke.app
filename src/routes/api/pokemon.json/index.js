d27c691251a5c2eeb1e301eff24aa67cea1da1dd
domtronn <dgc336@gmail.com>
2021-12-19 10:39:20 +0000

    [fix] Don't filter pokemon on every request

--- src/routes/api/pokemon.json/index.js
+++ src/routes/api/pokemon.json/index.js
@@ -1,8 +1,9 @@
 import Pokemon, { filterdata } from './_data.js'
+const items = filterdata(Pokemon)
 
 export async function get () {
   return {
-    body: filterdata(Pokemon),
+    body: items,
     headers: {
       'Cache-Control': 's-maxage=1, stale-while-revalidate',
       'Content-Type': 'application/json'
