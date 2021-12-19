a03e58bf0fb38c1ae2519cda815b715ad01cf6bf
domtronn <dgc336@gmail.com>
2021-12-19 07:57:31 +0000

    [fix] Add missinng file

---
+++ src/lib/utils/debounce.js
@@ -0,0 +1,9 @@
+export const debounce = (fn, ms = 0) => {
+  let timeoutId
+  return function (...args) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fn.apply(this, args), ms)
+  }
+}
+
+export default debounce
