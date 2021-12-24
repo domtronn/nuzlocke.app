997fafd354c0648314e72d43aca47c185b219475
domtronn <dgc336@gmail.com>
2021-12-24 22:11:30 +0000

    [fix] Add missing component

---
+++ src/lib/components/compare/Summary.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Tooltip } from '$lib/components/core'
+  export let pokemon
+</script>
+
+<span class='text-tiny flex gap-x-1'>
+  Lvl
+
+  <strong>{pokemon.level}</strong>
+
+  {#if pokemon.ability}
+    <strong class:cursor-help={pokemon.ability?.effect}>
+      {pokemon.ability.name}
+      {#if pokemon.ability.effect}
+        <span class=-ml-1>*</span>
+        <Tooltip>
+          {pokemon.ability.effect}
+        </Tooltip>
+      {/if}
+    </strong>
+  {/if}
+</span>
