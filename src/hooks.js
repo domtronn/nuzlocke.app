79818f45743ea16d5c4b5def2315386bbed7e3c5
domtronn <dgc336@gmail.com>
2022-01-22 14:57:10 +0000

    [feature] Add secruity headers with hooks

---
+++ src/hooks.js
@@ -0,0 +1,13 @@
+export async function handle ({ request, resolve }) {
+  const response = await resolve(request)
+
+  response.headers = {
+    ...response.headers,
+    'Content-Security-Policy': `default-src 'self'; img-src *; script-src nuzlocke.app nuzlocke.vercel.app nuzlocke-analytics.vercel.app`,
+    'X-Frame-Options': 'SAMEORIGIN',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Content-Type-Options': 'nosniff'
+  }
+
+  return response
+}
