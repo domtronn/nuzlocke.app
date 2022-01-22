70bd17bfce2644bd1f820fb4bdbf24da32530ad0
domtronn <dgc336@gmail.com>
2022-01-22 14:57:57 +0000

    [fix] Add nuzlocke beta to allowed script srcs

--- src/hooks.js
+++ src/hooks.js
@@ -3,7 +3,7 @@ export async function handle ({ request, resolve }) {
 
   response.headers = {
     ...response.headers,
-    'Content-Security-Policy': `default-src 'self'; img-src *; script-src nuzlocke.app nuzlocke.vercel.app nuzlocke-analytics.vercel.app`,
+    'Content-Security-Policy': `default-src 'self'; img-src *; script-src nuzlocke.app nuzlocke.vercel.app nuzlocke-beta.vercel.app nuzlocke-analytics.vercel.app`,
     'X-Frame-Options': 'SAMEORIGIN',
     'X-XSS-Protection': '1; mode=block',
     'X-Content-Type-Options': 'nosniff'
