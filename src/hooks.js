9294f8aa74ce170465d828e09a7e60e59949f793
domtronn <dgc336@gmail.com>
2022-01-24 22:30:09 +0000

    [fix] run on sprites

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,7 @@
 export async function handle ({ event, request, resolve }) {
   const response = await resolve(request)
 
-  if (request.path.startsWith('/new'))
+  if (request.path.startsWith('/sprites/'))
     response.headers = {
       ...response.headers,
       'Cache-Control': 'public, max-age=31536000, must-revalidate',
