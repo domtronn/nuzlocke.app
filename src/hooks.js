fd3bf4f9e4d5eaa2687fb130ccab5adc57e14be0
domtronn <dgc336@gmail.com>
2022-01-24 22:28:02 +0000

    [poc] add  hooks for response or proxy

---
+++ src/hooks.js
@@ -0,0 +1,12 @@
+export async function handle ({ event, request, resolve }) {
+  const response = await resolve(request)
+
+  if (request.path.startsWith('/new'))
+    response.headers = {
+      ...response.headers,
+      'Cache-Control': 'public, max-age=31536000, must-revalidate',
+      'X-Hooked': true
+    }
+
+  return response
+}
