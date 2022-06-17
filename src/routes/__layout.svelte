617f879c7fd63f6bb49481865d71d8ae0129997d
domtronn <dgc336@gmail.com>
2022-06-17 11:28:07 +0100

    Remove clarity and speedcurve due to CSP issues

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,49 +1,47 @@
-<script context='module'>
-  export function load ({ page }) {
-    return { props: { path: page.path } }
+<script context="module">
+  export function load({ page }) {
+    return { props: { path: page.path } };
   }
 </script>
 
 <script>
-  export let path = ''
+  export let path = '';
 
-  import '../app.css'
-  import { setContext } from 'svelte'
-  import { browser, dev } from '$app/env'
+  import '../app.css';
+  import { setContext } from 'svelte';
+  import { browser, dev } from '$app/env';
 
-  import { fetchData, fetchLeague } from '$utils/fetchers'
-  import { GameHeading, NavHeading, CookieBanner, Footer } from '$c/navs'
-  import Modal from 'svelte-simple-modal'
+  import { fetchData, fetchLeague } from '$utils/fetchers';
+  import { GameHeading, NavHeading, CookieBanner, Footer } from '$c/navs';
+  import Modal from 'svelte-simple-modal';
 
   setContext('game', {
     getLeague: fetchLeague,
     getAllPkmn: fetchData,
-    getPkmn: id => fetchData().then((ps = []) => ps.find(p => p.num == id || p.name == id || p.alias == id || p.sprite == id)),
-    getPkmns: (ids = []) => fetchData()
-      .then((ps = []) => ps
-            .filter(p => ids.includes(p.num) || ids.includes(p.name) || ids.includes(p.alias))
-            .reduce((acc, it) => ({ ...acc, [it.alias]: it }), {})),
-  })
+    getPkmn: (id) =>
+      fetchData().then((ps = []) =>
+        ps.find(
+          (p) => p.num == id || p.name == id || p.alias == id || p.sprite == id
+        )
+      ),
+    getPkmns: (ids = []) =>
+      fetchData().then((ps = []) =>
+        ps
+          .filter(
+            (p) =>
+              ids.includes(p.num) ||
+              ids.includes(p.name) ||
+              ids.includes(p.alias)
+          )
+          .reduce((acc, it) => ({ ...acc, [it.alias]: it }), {})
+      )
+  });
 
-  const title = 'Nuzlocke Tracker'
-  const subtitle = 'Track encounters, analyse your team, be prepared'
-  const description = 'Best application to track Pokémon encounters and prepare for every boss battle with details on stats, movesets, & abilities - never lose a Nuzlocke run again.'
+  const title = 'Nuzlocke Tracker';
+  const subtitle = 'Track encounters, analyse your team, be prepared';
+  const description =
+    'Best application to track Pokémon encounters and prepare for every boss battle with details on stats, movesets, & abilities - never lose a Nuzlocke run again.';
 </script>
-
-<style global lang="postcss">
-  :global(:root){ --input-bg: theme('colors.white'); }
-  :global(.dark) { --input-bg: theme('colors.gray.800'); }
-
-  /* latin */
-  @font-face {
-    font-family: 'VT323';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url(https://fonts.gstatic.com/s/vt323/v12/pxiKyp0ihIEF2isfFJXUdVNF.woff2) format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-  }
-</style>
 
 <svelte:head>
   <title>{title} | {subtitle}</title>
@@ -53,23 +51,18 @@
   <meta content={description} name="description" />
   <meta name="twitter:description" content={description} />
 
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   {#if !dev && browser}
-    <script async defer
-            data-website-id="dae73bab-b128-46a6-93e1-46ebc3da783c"
-            src="https://nuzlocke-analytics.vercel.app/umami.js">
-    </script>
-    <script type="text/javascript">
-      (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "ab4cx3385y");
+    <script
+      async
+      defer
+      data-website-id="dae73bab-b128-46a6-93e1-46ebc3da783c"
+      src="https://nuzlocke-analytics.vercel.app/umami.js">
     </script>
   {/if}
 </svelte:head>
 
-{#if ['/game', '/box'].includes(path) }
+{#if ['/game', '/box'].includes(path)}
   <GameHeading />
 {:else}
   <NavHeading />
@@ -86,5 +79,26 @@
 
 <CookieBanner />
 
-<iframe id='cdls' src='https://nuzlocke.vercel.app/iframe.html' title='Cross Domain LocalStorage migration interface' />
 <Footer />
+
+<style global lang="postcss">
+  :global(:root) {
+    --input-bg: theme('colors.white');
+  }
+  :global(.dark) {
+    --input-bg: theme('colors.gray.800');
+  }
+
+  /* latin */
+  @font-face {
+    font-family: 'VT323';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url(https://fonts.gstatic.com/s/vt323/v12/pxiKyp0ihIEF2isfFJXUdVNF.woff2)
+      format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
+      U+2215, U+FEFF, U+FFFD;
+  }
+</style>
