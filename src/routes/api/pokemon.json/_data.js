be27116b1d9c06f42cf06c49f843d30d1d89eeee
domtronn <dgc336@gmail.com>
2023-03-01 09:40:37 +0000

    [fix] Load static Pokemon JSON pulled from pokeapi

--- src/routes/api/pokemon.json/_data.js
+++ src/routes/api/pokemon.json/_data.js
@@ -1,4 +1,4 @@
-import Pokemon from 'pokemon-assets/assets/data/pokemon.json';
+import Pokemon from './_pokemon.json';
 import { filterObj, mapObj } from '$lib/utils/arr';
 import { pick } from 'ramda';
 
@@ -81,24 +81,21 @@ const findEvoLine = (p) => {
     : res?.sprite;
 };
 
-export const format = ({ name, forme }) => {
-  if (forme === 'Galar') return `Galarian ${name.replace(/-Galar/g, '')}`;
-  if (forme === 'Alola') return `Alolan ${name.replace(/-Alola/g, '')}`;
+export const format = ({ name, alias }) => {
+  if (alias.endsWith('-galar')) return `Galarian ${name}`;
+  if (alias.endsWith('-alola')) return `Alolan ${name}`;
+  if (alias.endsWith('-hisui')) return `Hisuian ${name}`;
+  if (alias.endsWith('-paldea')) return `Paldean ${name}`;
   return name;
 };
 
 export const sumObj = (o) => Object.values(o).reduce((acc, it) => acc + it, 0);
 
 export default mapObj(
-  filterObj(
-    Pokemon,
-    (pkmn) => !pkmn.forme || ['Galar', 'Alola'].includes(pkmn.forme)
-  ),
+  Pokemon,
   (pkmn) => ({
     ...pkmn,
-    imgId: FormMap[pkmn.alias] || pkmn.num,
-    total: sumObj(pkmn.baseStats),
-    evoline: findEvoLine(pkmn),
+    imgId: pkmn.imgId,
     label: format(pkmn)
   })
 );
