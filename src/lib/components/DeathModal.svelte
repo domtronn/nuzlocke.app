ea3dc15982cb93cf0d1f0768230b435670a6b896
domtronn <dgc336@gmail.com>
2023-03-17 14:59:47 +0000

    Work

--- src/lib/components/DeathModal.svelte
+++ src/lib/components/DeathModal.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let base, evolutions, select = _ => _, pokemon, nickname
+  export let evolutions, select = _ => _, pokemon, nickname
 
   import { capitalise } from '$lib/utils/string'
   import { Input, Button, IconButton, PIcon, AutoComplete, Picture } from '$lib/components/core'
@@ -16,18 +16,18 @@
   const [, key] = readdata()
 
   const TokenisedLossSentiments = [
-    `After sending in <POKEMON NAME> to battle, <POKEMON NAME> was shocked when their opponent's <OPPONENT POKEMON> easily defeated them.`,
-    `Despite their best efforts, <POKEMON NAME> was no match for the opponent's <OPPONENT POKEMON>, who easily took down the <POKEMON TYPE> type with a powerful <OPPONENT ATTACK> attack.`,
+    `After sending in <POKEMON NAME> to battle, <POKEMON NAME> was shocked when <OPPONENT NAME>'s <OPPONENT POKEMON> easily defeated them.`,
+    `Despite their best efforts, <POKEMON NAME> was no match for <OPPONENT NAME>'s <OPPONENT POKEMON>, who easily took down the <POKEMON NAME> with a powerful <OPPONENT ATTACK> attack.`,
     `<POKEMON NAME>'s overconfidence led them to underestimate a <OPPONENT POKEMON>, resulting in their humiliating death.`,
 
-    `In their battle against <OPPONENT NAME>, <POKEMON NAME> was quickly defeated by a well-placed <OPPONENT POKEMON> attack.`,
-    `Despite their confidence, <POKEMON NAME> was no match for <OPPONENT NAME>'s <OPONENT POKEMON>, who had superior speed and strength.`,
+    `In their battle against <OPPONENT NAME>, <POKEMON NAME> was quickly defeated by a well-placed attack from <OPPONENT POKEMON>.`,
+    `Despite their confidence, <POKEMON NAME> was no match for <OPPONENT NAME>'s <OPPONENT POKEMON>, who had superior speed and strength.`,
     `<POKEMON NAME> was easily taken out by <OPPONENT NAME>'s <OPPONENT POKEMON>, whose preparation and skill proved to be too much to handle.`,
-    `In a moment of desperation, <POKEMON NAME> used the wrong move against <OPPONENT NAME>'s <OPPONENT TYPE> type, resulting in a devastating kill.`,
+    `In a moment of desperation, <POKEMON NAME> used the wrong move against <OPPONENT NAME>'s <OPPONENT TYPE> Pokemon, resulting in a devastating kill.`,
 
-    `Despite their careful preparations, <POKEMON NAME> was no match for the devious <OPPONENT NAME> and his powerful <OPPONENT TYPE> type Pokemon.`,
+    `Despite their careful preparations, <POKEMON NAME> was no match for the devious <OPPONENT NAME> and their powerful <OPPONENT TYPE> Pokemon.`,
     `Relying on <POKEMON NAME> proved to be a mistake, as they were unable to best the diverse team of <OPPONENT NAME>.`,
-    `The trainer's underestimation of their opponent's unconventional team ended up costing the life of <POKEMON NAME> in their battle against <OPPONENT NAME>.`,
+    `The trainer's underestimation of <OPPONENT NAME>'s unconventional team ended up costing the life of <POKEMON NAME> in their battle against <OPPONENT NAME>.`,
     `Despite their best efforts, <POKEMON NAME> was unable to overcome the tricky strategies of the sly <OPPONENT POKEMON>.`,
 
     `<POKEMON NAME> got unlucky with their moves repeatedly missing leading to their death.`,
@@ -35,65 +35,79 @@
     `In a tense battle, <POKEMON NAME> died to <OPPONENT NAME>'s <OPPONENT POKEMON> who landed critical hit after critcal hit.`,
     `<POKEMON NAME> was doing well in the battle uutontil <OPPONENT NAME>'s <OPPONENT POKEMON> landed a critical hit, which knocked them out in one blow.`,
 
-    `In a desperate move, <POKEMON NAME> sacrificed themselves to allow the rest of their team to set up and sweep the <OPPONENT NAME>'s team.`,
+    `In a heroic display, <POKEMON NAME> sacrificed themselves to allow the rest of their team to set up and sweep <OPPONENT NAME>'s team.`,
     `<POKEMON NAME> knew that sacrificing themselves was necessary to give the team a better chance in future battles.`,
     `Sacrificing <POKEMON NAME> was a calculated decision to take out <OPPONENT NAME>'s <OPPONENT POKEMON> and secure the win.`,
-    `<POKEMON NAME> was sacrificed so that they could switch in a teammate with a type advantage and gain the upper hand.`,
+    `<POKEMON NAME> sacrificed themselves so that they could switch in a teammate with a type advantage and gain the upper hand.`,
     `Despite losing <POKEMON NAME>, they knew their sacrifice was the best move to make in the long run.`
   ]
 
   let random = l => l[Math.floor(Math.random() * l.length)]
   const format = (text) => {
-    console.log('formatting')
     return text
       .replace('<POKEMON NAME>', (nickname ? `${nickname} the ` : '') + pokemon.name )
       .replaceAll('<POKEMON NAME>', nickname || pokemon. name)
       .replaceAll('<POKEMON TYPE>', pokemon.types.join(' & '))
-      .replaceAll('<OPPONENT NAME>', selected?.boss || '<OPPONENT NAME>')
-      .replaceAll('<OPPONENT POKEMON>', opponent?.name || '<OPPONENT POKEMON>')
+      .replaceAll('<OPPONENT NAME>', selected?.boss || 'their opponent')
+      .replaceAll('<OPPONENT POKEMON>', opponent?.name || 'pokemon')
+      .replaceAll('<OPPONENT ATTACK>', attack?.name || 'attack')
+      .replaceAll('<OPPONENT TYPE>',
+                  getleader(league, selected)?.speciality + ' type'
+                  || opponent?.types[0] + ' type'
+                  || '')
+      .replace(/ +/g, ' ')
   }
 
   const onrandom = () => {
-    let temptext = text
-    text = format(random(TokenisedLossSentiments))
-    if (temptext === text) onrandom()
+    let temp = base
+    base = random(TokenisedLossSentiments)
+    if (temp === base) onrandom()
   }
 
-  let selected, opponent
+  const getleaders = (l) => [...new Set(l || [])]
+  const getleader = (league, boss) => league && boss ? league[boss.value] : null
+  const getmons = (league, boss) => league && boss ? league[boss.value]?.pokemon : null
+  const getatks = (mon) => mon?.moves
+
+  let selected, opponent, attack
+  let text, base
   $: getLeague(key).then(l => (league = l))
 
   $: opponent && getPkmn(opponent).then(console.log)
 
-  $: opponent && (text = format(text))
-  $: selected && (text = format(text))
-  $: text = format(random(TokenisedLossSentiments))
-  $: opponent && console.log('opponent', opponent)
+  $: attack && (text = format(base))
+  $: opponent && (text = format(base))
+  $: selected && (text = format(base))
+  $: base = random(TokenisedLossSentiments)
+  $: text = format(base)
+
+  $: opponent && console.log(opponent)
 
 </script>
 
-<section class='flex flex-col max-w-[45ch] text-center items-center dark:bg-gray-900 bg-gray-50 rounded-lg p-8 pb-12 mx-auto shadow-lg  dark:text-gray-200'>
+<section class='flex flex-col max-w-[45ch] text-center items-center dark:bg-gray-900 bg-gray-50 rounded-lg p-4 md:p-8 pb-12 mx-auto shadow-lg  dark:text-gray-200'>
   <h2 class='text-2xl '>R.I.P. {(nickname ? (nickname + ' the ') : '') + pokemon.name}</h2>
 
   <div class='h-px w-full my-4 dark:bg-gray-500 bg-gray-500' />
 
-    <form class='w-full gap-x-1 flex'>
+    <form class='w-full gap-2 flex flex-col mb-4'>
       <AutoComplete
         className='flex-1 w-full'
         rounded
         placeholder=Opponent
-        items={[...new Set(gyms || [])]}
+        items={getleaders(gyms)}
         label={i => i.boss. trim()}
         bind:selected
-        >
-        <span class='w-full inline-flex justify-between items-center h-8 px-4 py-5 md:py-6 '
+      >
+        <span class='w-full inline-flex justify-between items-center text-sm md:text-base h-4 md:h-8 px-4 py-4 md:py-6 '
               slot=item let:item let:label>
-          <span>{@html label}</span>
+          <span class='line-clamp-1 text-left'>{@html label}</span>
           {#if league}
           <Picture
             src='https://img.nuzlocke.app/{league[item.value].img?.src || league[item.value].img}'
             alt={item.name}
             pixelated
-            className='w-18 translate-x-1/4 float-right'
+            className='w-16 md:w-18 translate-x-1/4 float-right'
             aspect=72x52
             />
           {/if}
@@ -104,7 +118,7 @@
         className='flex-1 w-full'
         rounded
         placeholder=Pokemon
-        items={league && selected ? league[selected.value].pokemon : null}
+        items={getmons(league, selected)}
         label={i => i.name}
         bind:selected={opponent}
         >
@@ -120,11 +134,11 @@
         disabled
         rounded
         placeholder=Attack
-        items={league && selected ? league[selected.value].pokemon : null}
+        items={getatks(opponent)}
         label={i => i.name}
-        bind:selected={opponent}
+        bind:selected={attack}
         >
-        <span class='w-full inline-flex justify-between items-center h-8 px-4 py-5 md:py-6 '
+        <span class='w-full inline-flex justify-between items-center md:h-4 h-8 px-4 py-5 md:py-6 '
               slot=item let:item let:label>
           <span>{@html label}</span>
         </span>
@@ -133,7 +147,7 @@
 
     </form>
 
-  <p class='text-xl bg-gray-800 px-4 py-6 rounded-lg w-full relative'>
+  <p class='text-base md:text-xl bg-gray-800 px-2 py-3 md:px-4 md:py-6 rounded-lg w-full relative'>
     <PIcon
       className='grayscale transform scale-200 mb-4 -mt-2'
       name={pokemon.alias}
@@ -175,14 +189,6 @@
 </section>
 
 <style>
-  @keyframes shake {
-    0%, 50% { transform: scale(2.2) rotate(0);}
-    5%, 45% { transform: scale(2.2) rotate(-2deg);}
-    10%, 40% { transform: scale(2.2) rotate(2deg);}
-    15%, 25%, 35% { transform: scale(2.2) rotate(-5deg);}
-    20%, 30% { transform: scale(2.2) rotate(5deg);}
-  }
-
   button {
     transform: scale(2);
     transition: transform 0.2s ease;
