ba66f87b68526fd260dd159cd0b0ad0bdf96f2a3
domtronn <dgc336@gmail.com>
2023-03-17 09:04:28 +0000

    [fix] Set lazy loading on picture elements to improve load times

--- src/lib/components/core/Picture.svelte
+++ src/lib/components/core/Picture.svelte
@@ -5,10 +5,16 @@
   const [width, height] = aspect.split('x')
 </script>
 
-<picture in:fade>
-  <source onError="this.onerror = null; this.style.display = 'none'" srcset='{src}.webp' type='image/webp' />
-  <img class={className} class:pixelated={pixelated} src='{src}.png' {width} {height} {alt} {role} onerror='this.onerror = null; this.parentNode.children[0].srcset = this.parentNode.children[1].srcset = this.src'/>
-</picture>
+  <picture in:fade>
+    <source onError="this.onerror = null; this.style.display = 'none'" srcset='{src}.webp' type='image/webp' />
+    <img class={className}
+         class:pixelated={pixelated}
+         loading='lazy'
+         src='{src}.png'
+         {width} {height} {alt} {role}
+         onerror='this.onerror = null; this.parentNode.children[0].srcset = this.parentNode.children[1].srcset = this.src'
+         />
+  </picture>
 
 <style lang="postcss">
   img.pixelated {
