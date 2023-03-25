32f0704cad632625447295de994d050183d94f9e
domtronn <dgc336@gmail.com>
2023-03-25 06:44:58 +0000

    [fix] Massively reduce volume of audio

--- src/lib/components/Audio.svelte
+++ src/lib/components/Audio.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from 'svelte'
   export let src
 
   let audio
@@ -11,10 +12,15 @@
       played = true
     } catch (e) {}
   }
+
+  onMount(() => {
+    document.getElementById('audio-player').volume = 0.0075
+  })
+
 </script>
 
 <svelte:window on:mousemove={oninteraction} />
 
-<audio class='transform scale-75 mx-auto {$$restProps.class || ''}' bind:this={audio} controls autoplay loop id='audio'>
+<audio class='transform scale-75 mx-auto {$$restProps.class || ''}' bind:this={audio} controls autoplay loop id=audio-player>
   <source {src}>
 </audio>
