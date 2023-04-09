b023443d0f84f36bea82593b9c97687baefc3f4b
domtronn <dgc336@gmail.com>
2023-04-09 16:42:38 +0100

    [fix] Add missing code omponent

---
+++ src/lib/components/qr/TextCode.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+
+  export let code
+  let vals = [], refs = []
+
+  $: code = vals.filter(i => i).join('')
+  $: if (code.length === 8) {
+    dispatch('code', { code })
+  }
+
+  const nextref = i => e => {
+    e.data === null
+      ? refs[Math.max(i - 1, 0)].focus()
+      : refs[Math.min(i + 1, 7)].focus()
+  }
+
+</script>
+
+<div class='inline-flex gap-x-2'>
+  {#each Array(8).fill() as _, i}
+    <input
+      class='w-12 h-16 text-4xl !p-0 font-bold text-center uppercase rounded-lg dark:bg-gray-900 text-white !p-0'
+      maxlength=1
+      on:input={nextref(i)}
+      bind:this={refs[i]}
+      bind:value={vals[i]}
+      />
+    {/each}
+</div>
