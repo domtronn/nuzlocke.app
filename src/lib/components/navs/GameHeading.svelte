076667b1eebce9e4058df4fa5b5fa7b0f05f1b8a
domtronn <dgc336@gmail.com>
2023-04-10 08:09:24 +0100

    [feature] Add attempt count next to the reset button

--- src/lib/components/navs/GameHeading.svelte
+++ src/lib/components/navs/GameHeading.svelte
@@ -100,7 +100,7 @@
 
             <li class='px-4 py-3 text-center'>
               <Button on:click={reset} rounded solid class='text-sm w-full'>New Attempt</Button>
-              <p class='italic text-xs pt-2 opacity-50 leading-4'>Abandon your <b>{game.name}</b> run and reset all encounters</p>
+              <p class='italic text-xs pt-2 opacity-50 leading-4'>Abandon your <b>{game.name}</b> run, reset all encounters and start attempt {+game.attempts + 1}</p>
             </li>
           </ul>
         </Popover>
