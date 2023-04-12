257e2777c32a2784d11d6dba4292eac4610a7f2a
domtronn <dgc336@gmail.com>
2023-04-12 16:33:52 +0100

    [fix] Fix bug where you were unable to remove custom location mons from team

--- src/lib/components/TeamBuilder/mini-team-controller.svelte
+++ src/lib/components/TeamBuilder/mini-team-controller.svelte
@@ -17,7 +17,7 @@
     gameStore.subscribe(read(data => {
       seenTeam = Object.hasOwnProperty.call(data, '__team')
       teamData = readTeam(data)
-      boxData = data
+      boxData = readBox(data).reduce((acc, it) => ({ ...acc, [locid({detail: {data: it}})]: it }), {})
       boxLength = readBox(data).length
     }))
 
