42342240579ab926cf1074d08e343888a661ef04
domtronn <dgc336@gmail.com>
2023-04-17 08:38:48 +0100

    [feature] Paginate the box to improve space for mobile

--- src/lib/components/BossBattle/BoxTeam.svelte
+++ src/lib/components/BossBattle/BoxTeam.svelte
@@ -3,7 +3,9 @@
   import { crossfade } from 'svelte/transition'
   import { flip } from 'svelte/animate'
 
-  import { PIcon } from '$c/core'
+  import { PIcon, Icon } from '$c/core'
+  import { Arrow, Pip } from '$icons'
+  import { chunk } from '$lib/utils/arr'
 
   const [send, receive] = crossfade({ duration: 200 })
   const dispatch = createEventDispatcher()
@@ -14,9 +16,20 @@
     boss = {}
 
   $: teamList = team.map((t) => locid(t.original))
-  $: boxList = box.filter((i) => !teamList.includes(locid(i.original)))
+  $: boxList = box
+    .filter((i) => !teamList.includes(locid(i.original)))
+    .sort((a, b) => b.total - a.total)
 
-  //  TODO: Paginate the box
+  let page = 0,
+    pages,
+    max
+  $: (pages = chunk(boxList, 8 * 3)), (max = pages.length - 1)
+
+  $: if (page > max) page = max
+
+  const inc = (_) => (page = Math.min(max, page + 1))
+  const dec = (_) => (page = Math.max(0, page - 11))
+  const set = (i) => (_) => (page = i)
 
   const select = (poke) => () => dispatch('select', poke)
   const clear = () => dispatch('clear')
@@ -69,28 +82,41 @@
 
   <div>
     <h2>Box</h2>
-    <p>
-      Select pokemon to build a team against {boss.name}
-    </p>
+    <div class="flex items-center">
+      <p class="flex-1">Pick Pokemon from your boxes to face {boss.name}</p>
+
+      {#each Array(pages.length) as p, i}
+        <button
+          disabled={page === i}
+          class="pip"
+          title="Box {i + 1}"
+          on:click={set(i)}
+        >
+          <Icon inline="true" height="0.5rem" icon={Pip} />
+        </button>
+      {/each}
+    </div>
     <ul
       class="mt-2 grid w-fit grid-cols-8 rounded-lg bg-gray-200 px-4 py-2 dark:bg-gray-800"
     >
-      {#each boxList as poke (locid(poke.original))}
-        {@const og = poke.original}
-        <button
-          title={team.length === 6
-            ? 'Your team is full!'
-            : `Add ${og.pokemon} to your team`}
-          class="relative mx-auto h-10 w-10"
-          disabled={team.length === 6}
-          on:click={select(og)}
-          animate:flip={{ duration: 200 }}
-          in:receive={{ key: locid(og) }}
-          out:send={{ key: locid(og) }}
-        >
-          <PIcon class="picon" name={og.pokemon} />
-        </button>
-      {/each}
+      {#key page}
+        {#each pages[page] as poke (locid(poke.original))}
+          {@const og = poke.original}
+          <button
+            title={team.length === 6
+              ? 'Your team is full!'
+              : `Add ${og.pokemon} to your team`}
+            class="relative mx-auto h-10 w-10"
+            disabled={team.length === 6}
+            on:click={select(og)}
+            animate:flip={{ duration: 200 }}
+            in:receive={{ key: locid(og) }}
+            out:send={{ key: locid(og) }}
+          >
+            <PIcon class="picon" name={og.pokemon} />
+          </button>
+        {/each}
+      {/key}
     </ul>
   </div>
 </div>
@@ -116,7 +142,9 @@
     @apply grayscale;
   }
 
-  button.clear::before {
+  button.clear::before,
+  button.page::before,
+  button.pip::before {
     content: none;
   }
 
@@ -160,5 +188,23 @@
 
   button {
     @apply transition;
+  }
+
+  button.page {
+    @apply z-50 text-gray-600 transition hover:text-orange-600 disabled:cursor-default disabled:hover:text-gray-600;
+  }
+
+  :global(.dark) button.page {
+    @apply text-gray-200 hover:text-orange-500 disabled:hover:text-gray-200;
+  }
+
+  .pip:disabled {
+    @apply pointer-events-none scale-150 opacity-100;
+  }
+  .pip {
+    @apply p-1 opacity-50;
+  }
+  .pip:enabled:hover {
+    @apply text-orange-500 opacity-100;
   }
 </style>
