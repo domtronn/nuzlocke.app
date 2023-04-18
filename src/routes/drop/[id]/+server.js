20ce74d2faa955f3c7ffc8ee4d479cf923d2114f
domtronn <dgc336@gmail.com>
2023-04-18 20:21:31 +0100

    [fix] Add logging to the drop endpoint to debug 5XXs

--- src/routes/drop/[id]/+server.js
+++ src/routes/drop/[id]/+server.js
@@ -6,14 +6,21 @@ const opts = {
   port: 443
 }
 
+const log = (msg) => console.log(`GET /drop ${msg}`)
+
 export async function GET ({ request, params }) {
   const { host } = new URL(request.url)
-  
+
+  log('host :' + host)
+  log('id   :' + params.id)
+
   return new Promise(resolve => {
     const req = https.request(
       { ...opts, path: '/' + params.id },
       res => {
         let { location } = res.headers
+
+        log('loaction :' + location)
 
         if (host.startsWith('localhost')) {
           location = location.replace(/.*?\/drop/, `http://${host}/drop`)
