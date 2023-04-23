77ae43f2a7d7946412aa24283e2bd1949a02d212
domtronn <dgc336@gmail.com>
2023-04-23 14:36:13 +0100

    [feature] Add debug feature to print out save of LocalStorage save files

--- src/lib/store.js
+++ src/lib/store.js
@@ -487,6 +487,22 @@ fixDupes()
 
 if (typeof window !== 'undefined')
   window.nz = {
+    size: function () {
+      var _lsTotal = 0,
+        _xLen,
+        _x
+      for (_x in localStorage) {
+        if (!localStorage.hasOwnProperty(_x)) {
+          continue
+        }
+        _xLen = (localStorage[_x].length + _x.length) * 2
+        _lsTotal += _xLen
+        console.log(
+          _x.substr(0, 50) + ' = ' + (_xLen / 1024).toFixed(2) + ' KB'
+        )
+      }
+      console.log('Total = ' + (_lsTotal / 1024).toFixed(2) + ' KB')
+    },
     saves: function () {
       return [
         window.localStorage.getItem(IDS.saves),
