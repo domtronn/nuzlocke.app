406cc5309dcfd9db0eb93074309096b44aedaa68
domtronn <dgc336@gmail.com>
2023-04-24 18:09:15 +0100

    [fix] Remove the v timestamp

--- src/lib/utils/fetchers.js
+++ src/lib/utils/fetchers.js
@@ -8,10 +8,11 @@ export const fetchData = async () => {
   if (!browser) return
 
   const gen = await getGen()
-  const uri = `${DATA}/pokemon/${gen}.json?v=14042023`
-  if (data[gen]) return data[gen]        // Return the raw data if it exists
-  if (!data[uri]) data[uri] = fetch(uri) // "Cache" the promise rather than make a new fetch each time
-    .then(res => res.json())
+  const uri = `${DATA}/pokemon/${gen}.json`
+  if (data[gen]) return data[gen] // Return the raw data if it exists
+  if (!data[uri])
+    data[uri] = fetch(uri) // "Cache" the promise rather than make a new fetch each time
+      .then((res) => res.json())
 
   data[gen] = await data[uri]
   return data[gen]
@@ -22,10 +23,9 @@ export const fetchLeague = async (game, starter = 'fire') => {
   if (!browser) return
 
   const id = `${game}@${starter}`
-  const uri = `${DATA}/league/${game}.${starter}.json?v=14042023`
+  const uri = `${DATA}/league/${game}.${starter}.json`
   if (league[id]) return league[id]
-  if (!league[uri]) league[uri] = fetch(uri)
-    .then(res => res.json())
+  if (!league[uri]) league[uri] = fetch(uri).then((res) => res.json())
 
   league[id] = await league[uri]
   return league[id]
@@ -35,10 +35,9 @@ const route = {}
 export const fetchRoute = async (game) => {
   if (!browser) return
 
-  const uri = `/api/route/${game}.json?v=14042023`
+  const uri = `/api/route/${game}.json`
   if (route[game]) return route[game]
-  if (!route[uri]) route[uri] = fetch(uri)
-    .then(res => res.json())
+  if (!route[uri]) route[uri] = fetch(uri).then((res) => res.json())
 
   route[game] = await route[uri]
   return route[game]
@@ -48,10 +47,9 @@ const trainers = {}
 export const fetchTrainers = async (game) => {
   if (!browser) return
 
-  const uri = `/api/${game}/trainers.json?v=14042023`
+  const uri = `/api/${game}/trainers.json`
   if (trainers[game]) return trainers[game]
-  if (!trainers[uri]) trainers[uri] = fetch(uri)
-    .then(res => res.json())
+  if (!trainers[uri]) trainers[uri] = fetch(uri).then((res) => res.json())
 
   trainers[game] = await trainers[uri]
   return trainers[game]
