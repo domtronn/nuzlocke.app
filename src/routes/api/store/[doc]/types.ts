70616e6837781e4e0f600740aea502ee55146e9f
domtronn <dgc336@gmail.com>
2023-04-24 15:28:11 +0100

    [data] Add Blaze Black / Volt White to valid game types

--- src/routes/api/store/[doc]/types.ts
+++ src/routes/api/store/[doc]/types.ts
@@ -1,20 +1,23 @@
-import z from "zod"
+import z from 'zod'
 
 const EGame = z.enum([
-    "rrss",
-    "emrunbun",
-    'unbound', 'unbound_exp',
-    'radred_hard',
-    'emkaizo',
-    'vi', 'sc', 'sw', 'sh', 'usun', 'umoon', 'sun',
-    'moon', 'x', 'y', 'blazevolt2', 'blazevolt2_c', 'bl2', 'wh2', 'bl2c',
-    'wh2c', 'bl', 'wh', 'bd', 'sp', 'radred', 'renplat', 'pt', 'd', 'p',
-    'or', 'as', 'em', 'incem', 'ruby', 'saph', 'stormsilv', 'sacredgold',
-    'hg', 'ss', 'gold', 'silv', 'crys', 'fr', 'lg', 'yel', 'red', 'blue'
+    'blaze', 'volt', 'rrss', 'emrunbun', 'unbound',
+    'unbound_exp', 'radred_hard', 'emkaizo', 'vi', 'sc', 'sw', 'sh',
+    'usun', 'umoon', 'sun', 'moon', 'x', 'y', 'blazevolt2',
+    'blazevolt2_c', 'bl2', 'wh2', 'bl2c', 'wh2c', 'bl', 'wh', 'bd',
+    'sp', 'radred', 'renplat', 'pt', 'd', 'p', 'or', 'as', 'em',
+    'incem', 'ruby', 'saph', 'stormsilv', 'sacredgold', 'hg', 'ss',
+    'gold', 'silv', 'crys', 'fr', 'lg', 'yel', 'red', 'blue'
 ])
 const EDeath = z.enum(['boss', 'encounter', 'trainer'])
 const ECategory = z.enum(['random', 'mistake', 'sacrifice', 'drama', 'luck'])
-const EBossType = z.enum(['gym-leader', 'elite-four', 'evil-team', 'rival', 'mini-boss'])
+const EBossType = z.enum([
+  'gym-leader',
+  'elite-four',
+  'evil-team',
+  'rival',
+  'mini-boss'
+])
 
 const IGame = z.object({
   game: EGame,
@@ -23,63 +26,73 @@ const IGame = z.object({
   settings: z.string().regex(/[01]+/),
   attempts: z.optional(z.coerce.number()),
   created_at: z.coerce.number(),
-  updated_at: z.optional(z.coerce.number()),
+  updated_at: z.optional(z.coerce.number())
 })
 
 const ITeamMember = z.object({
-    position: z.number().min(1).max(6),
-    location: z.string(),
-    pokemon: z.string(),
+  position: z.number().min(1).max(6),
+  location: z.string(),
+  pokemon: z.string()
 })
 
 export const ITeams = z.object({
-    game_id: z.string().uuid(),
-    user_id: z.string().uuid(),
-    data: z.array(ITeamMember)
+  game_id: z.string().uuid(),
+  user_id: z.string().uuid(),
+  data: z.array(ITeamMember)
 })
 
 export const IFight = z.object({
-    boss_id: z.string(),
-    boss_name: z.string(),
-    boss_speciality: z.optional(z.string()),
-    boss_type: EBossType,
-    team: z.array(ITeamMember)
+  boss_id: z.string(),
+  boss_name: z.string(),
+  boss_speciality: z.optional(z.string()),
+  boss_type: EBossType,
+  team: z.array(ITeamMember)
 })
 
 export const IFights = z.object({
-    game_id: z.string().uuid(),
-    user_id: z.string().uuid(),
-    data: z.array(IFight)
+  game_id: z.string().uuid(),
+  user_id: z.string().uuid(),
+  data: z.array(IFight)
 })
 
 const IDeath = z.object({
-    type: z.optional(EDeath),
-    category: z.optional(ECategory),
-    time: z.optional(z.number()),
-    custom: z.optional(z.boolean()),
-    epitaph: z.optional(z.string()),
-    lvl: z.optional(z.object({
-        from: z.optional(z.number().min(1).max(100)),
-        to: z.optional(z.number().min(1).max(100))
-    })),
-    location: z.optional(z.object({
-        name: z.optional(z.string())
-    })),
-    opponent: z.optional(z.object({
-        name: z.optional(z.string()),
-        id: z.optional(z.string()),
-        types: z.optional(z.array(z.string()).min(1).max(2)),
-    })),
-    trainer: z.optional(z.object({
-        id: z.optional(z.string()),
-        name: z.optional(z.string()),
-        type: z.optional(z.string()),
-        speciality: z.optional(z.string()),
-    })),
-    attack: z.optional(z.object({
-        name: z.optional(z.string()),
-        type: z.string()
-    }))
+  type: z.optional(EDeath),
+  category: z.optional(ECategory),
+  time: z.optional(z.number()),
+  custom: z.optional(z.boolean()),
+  epitaph: z.optional(z.string()),
+  lvl: z.optional(
+    z.object({
+      from: z.optional(z.number().min(1).max(100)),
+      to: z.optional(z.number().min(1).max(100))
+    })
+  ),
+  location: z.optional(
+    z.object({
+      name: z.optional(z.string())
+    })
+  ),
+  opponent: z.optional(
+    z.object({
+      name: z.optional(z.string()),
+      id: z.optional(z.string()),
+      types: z.optional(z.array(z.string()).min(1).max(2))
+    })
+  ),
+  trainer: z.optional(
+    z.object({
+      id: z.optional(z.string()),
+      name: z.optional(z.string()),
+      type: z.optional(z.string()),
+      speciality: z.optional(z.string())
+    })
+  ),
+  attack: z.optional(
+    z.object({
+      name: z.optional(z.string()),
+      type: z.string()
+    })
+  )
 })
 
 const ILocation = z.object({
