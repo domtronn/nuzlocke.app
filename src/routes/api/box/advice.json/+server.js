70165ae87eeff13635a7185220e20d3aa127477a
domtronn <dgc336@gmail.com>
2023-04-26 14:00:18 +0100

    [fix] Remove old advice API and replace with util functions

--- src/routes/api/box/advice.json/+server.js
+++ src/routes/api/box/advice.json/+server.js
@@ -1,6 +1,6 @@
-import { types } from '$lib/data/types';
-import { moveResistance } from '../../battle/advice.json/_type-advice';
-import { typeAdvantages } from '../../battle/advice.json/_types';
+import { types } from '$lib/data/types'
+import { moveResistance } from '$utils/advice/advisor-types'
+import { typeAdvantages } from '$utils/advice/types'
 
 /**
  * Calculates the box coverage for types
@@ -8,19 +8,19 @@ import { typeAdvantages } from '../../battle/advice.json/_types';
  */
 
 export async function POST({ body }) {
-  const { box } = body;
+  const { box } = body
   const advice = types.reduce((acc, it) => {
-    const dmg = box.map(({ types }) => moveResistance(it, types));
+    const dmg = box.map(({ types }) => moveResistance(it, types))
     const minDmg = dmg.reduce((acc, it) => {
-      const id = it < 1 ? 'resist' : it > 1 ? 'weak' : 'neutral';
+      const id = it < 1 ? 'resist' : it > 1 ? 'weak' : 'neutral'
       return {
         ...acc,
         [id]: (+acc[id] || 0) + 1
-      };
-    }, {});
+      }
+    }, {})
 
-    return { ...acc, [it]: minDmg };
-  }, {});
+    return { ...acc, [it]: minDmg }
+  }, {})
 
   const suggestMap = Object.entries(advice)
     .filter(([, { resist }]) => !resist)
@@ -28,7 +28,7 @@ export async function POST({ body }) {
       return [
         ...(typeAdvantages[type]['0.5'] || []),
         ...(typeAdvantages[type]['0'] || [])
-      ];
+      ]
     })
     .flat()
     .reduce(
@@ -37,11 +37,11 @@ export async function POST({ body }) {
         [it]: (acc[it] || 0) + 1
       }),
       {}
-    );
+    )
 
   const suggestions = Object.entries(suggestMap)
     .sort(([, a], [, b]) => b - a)
-    .map(([t]) => t);
+    .map(([t]) => t)
 
-  return new Response(JSON.stringify({ advice, suggestions }));
+  return new Response(JSON.stringify({ advice, suggestions }))
 }
