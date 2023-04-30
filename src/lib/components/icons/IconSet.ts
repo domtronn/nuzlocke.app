571d969a996483099193306c4abf150c18508fa5
domtronn <dgc336@gmail.com>
2023-04-30 07:03:33 +0100

    [feature] Add download button to the save component

--- src/lib/components/icons/IconSet.ts
+++ src/lib/components/icons/IconSet.ts
@@ -1,140 +1,116 @@
 // Core icon component
-export * as Icon from '@iconify/svelte/dist/OfflineIcon.svelte';
-
-// Navigation Icons
-export { default as Arrow } from '@iconify-icons/gg/arrow-right';
-export { default as ArrowToTop } from '@iconify-icons/gg/push-up';
-
-export { default as DoubleChevron } from '@iconify-icons/gg/chevron-double-up';
-export { default as Chevron } from '@iconify-icons/gg/chevron-up';
-
-export { default as Home } from '@iconify-icons/ri/home-4-line';
-export { default as Menu } from '@iconify-icons/ri/menu-fill';
-export { default as Search } from '@iconify-icons/gg/search';
-
-// UX Icons - Things like close/trash
-export { default as Bin } from '@iconify-icons/gg/trash';
-export { default as Trash } from '@iconify-icons/gg/trash';
-
-export { default as Boost } from '@iconify-icons/ri/arrow-up-s-fill';
-
-export { default as Delete } from '@iconify-icons/ri/delete-back-2-line';
-export { default as Dots } from '@iconify-icons/ph/dots-three-vertical-bold';
-export { default as Caret } from '@iconify-icons/ph/caret-down-fill'; // Previous DownArr
-export { default as CaretRight } from '@iconify-icons/ph/caret-right-fill'; // Previous RightArr
-export { default as X } from '@iconify-icons/ri/close-fill';
-export { default as Edit } from '@iconify-icons/ph/pencil-simple';
-export { default as Error } from '@iconify-icons/ph/warning-octagon';
-export { default as External } from '@iconify-icons/gg/external';
-export { default as Loop } from '@iconify-icons/fe/loop';
-export { default as Hide } from '@iconify-icons/ph/eye-slash';
-export { default as Info } from '@iconify-icons/ri/information-line';
-export { default as Link } from '@iconify-icons/ph/link';
-
-export { default as Off } from '@iconify-icons/gg/radio-check';
-export { default as On } from '@iconify-icons/gg/radio-checked';
-
-export { default as Settings } from '@iconify-icons/ri/settings-4-line';
-export { default as Share } from '@iconify-icons/ri/share-forward-line';
-export { default as Spinner } from '@iconify-icons/gg/spinner';
-
-export { default as Sort } from '@iconify-icons/ph/sort-ascending';
-export { default as Unfilter } from '@iconify-icons/ri/filter-off-fill';
-
-// Nuzlocke states
-export { default as Gift } from '@iconify-icons/ph/gift';
-export { default as Traded } from '@iconify-icons/gg/arrows-exchange';
-export { default as Missed } from '@iconify-icons/ri/windy-line';
-
-export { default as LongGrass } from '@iconify-icons/mdi/grass';
-
+export { default as Deceased } from '@iconify-icons/emojione-monotone/skull-and-crossbones'
+export { default as Spd } from '@iconify-icons/fa-solid/brain'
+export {
+  default as Atk,
+  default as Physical
+} from '@iconify-icons/fa-solid/fist-raised'
+export { default as Hand } from '@iconify-icons/fa-solid/hand-holding'
+export { default as Loop } from '@iconify-icons/fe/loop'
+export { default as Spa } from '@iconify-icons/fe/target'
 // UI Icons - Things like hearts & visual
-export { default as Add } from '@iconify-icons/gg/add-r';
-export { default as Ball } from '@iconify-icons/gg/pokemon';
-export { default as Box } from '@iconify-icons/ph/package';
-export { default as Deceased } from '@iconify-icons/emojione-monotone/skull-and-crossbones';
-export { default as Analysis } from '@iconify-icons/ri/test-tube-fill';
-export { default as Bubbles } from '@iconify-icons/ri/bubble-chart-fill';
-
-export { default as Floppy } from '@iconify-icons/ph/floppy-disk';
-export { default as Save } from '@iconify-icons/ph/floppy-disk';
-export { default as QR } from '@iconify-icons/gg/qr';
-export { default as QRCode } from '@iconify-icons/ri/qr-code-fill';
-
-export { default as Quote } from '@iconify-icons/ri/double-quotes-l'
-
+export { default as Add } from '@iconify-icons/gg/add-r'
+// Navigation Icons
+export { default as Arrow } from '@iconify-icons/gg/arrow-right'
+export {
+  default as Traded,
+  default as Transfer
+} from '@iconify-icons/gg/arrows-exchange'
+export { default as DoubleChevron } from '@iconify-icons/gg/chevron-double-up'
+export { default as Chevron } from '@iconify-icons/gg/chevron-up'
+export { default as External } from '@iconify-icons/gg/external'
+export { default as File } from '@iconify-icons/gg/file-add'
+export { default as Game } from '@iconify-icons/gg/games'
+export { default as Minus } from '@iconify-icons/gg/math-minus'
+export { default as Plus } from '@iconify-icons/gg/math-plus'
+export { default as Ball } from '@iconify-icons/gg/pokemon'
+export { default as ArrowToTop } from '@iconify-icons/gg/push-up'
+export { default as QR } from '@iconify-icons/gg/qr'
+export { default as Off } from '@iconify-icons/gg/radio-check'
+export { default as On } from '@iconify-icons/gg/radio-checked'
+export { default as Search } from '@iconify-icons/gg/search'
+export { default as Spinner } from '@iconify-icons/gg/spinner'
+// UX Icons - Things like close/trash
+export { default as Bin, default as Trash } from '@iconify-icons/gg/trash'
 export { default as Dice } from '@iconify-icons/mdi/dice-multiple-outline'
-
+export { default as LongGrass } from '@iconify-icons/mdi/grass'
+export { default as Caret } from '@iconify-icons/ph/caret-down-fill' // Previous DownArr
+export { default as CaretRight } from '@iconify-icons/ph/caret-right-fill' // Previous RightArr
+export { default as Pip } from '@iconify-icons/ph/circle-fill'
+export { default as Check } from '@iconify-icons/ph/circle-wavy-check-fill'
+export { default as Cookie } from '@iconify-icons/ph/cookie-fill'
+export { default as Desktop } from '@iconify-icons/ph/desktop'
+export { default as Mobile } from '@iconify-icons/ph/device-mobile'
+export { default as Dots } from '@iconify-icons/ph/dots-three-vertical-bold'
+export { default as Water } from '@iconify-icons/ph/drop-fill'
+export { default as Hide } from '@iconify-icons/ph/eye-slash'
+export {
+  default as Floppy,
+  default as Save
+} from '@iconify-icons/ph/floppy-disk'
+// Nuzlocke states
+export { default as Gift } from '@iconify-icons/ph/gift'
+export {
+  default as HP,
+  default as Heart
+} from '@iconify-icons/ph/heart-straight-fill'
+export { default as Spe } from '@iconify-icons/ph/lightning-fill'
+export { default as Link } from '@iconify-icons/ph/link'
+export { default as Map } from '@iconify-icons/ph/map-trifold'
+// Light & Dark mode
+export { default as DarkMode } from '@iconify-icons/ph/moon-fill'
+export { default as Box } from '@iconify-icons/ph/package'
+export { default as Edit } from '@iconify-icons/ph/pencil-simple'
+export { default as Sort } from '@iconify-icons/ph/sort-ascending'
+export { default as LightMode } from '@iconify-icons/ph/sun-fill'
+export { default as Special } from '@iconify-icons/ph/target'
+export { default as Error } from '@iconify-icons/ph/warning-octagon'
+export { default as Status } from '@iconify-icons/ph/yin-yang-fill'
+export { default as Boost } from '@iconify-icons/ri/arrow-up-s-fill'
+export { default as Asterisk } from '@iconify-icons/ri/asterisk' // Previously info
+export { default as BarChart } from '@iconify-icons/ri/bar-chart-2-fill'
+export { default as Bubbles } from '@iconify-icons/ri/bubble-chart-fill'
+export { default as X } from '@iconify-icons/ri/close-fill'
+export { default as Delete } from '@iconify-icons/ri/delete-back-2-line'
+export { default as Quote } from '@iconify-icons/ri/double-quotes-l'
+export { default as Unfilter } from '@iconify-icons/ri/filter-off-fill'
+// Type & Stat Icons
+export { default as Fire } from '@iconify-icons/ri/fire-fill'
+export { default as Track } from '@iconify-icons/ri/footprint-fill'
+export { default as Home } from '@iconify-icons/ri/home-4-line'
+export { default as Info } from '@iconify-icons/ri/information-line'
+export { default as Grass } from '@iconify-icons/ri/leaf-fill'
+export { default as Menu } from '@iconify-icons/ri/menu-fill'
+export { default as QRCode } from '@iconify-icons/ri/qr-code-fill'
+export { default as Settings } from '@iconify-icons/ri/settings-4-line'
+export { default as Share } from '@iconify-icons/ri/share-forward-line'
+export { default as Def } from '@iconify-icons/ri/shield-fill'
+export { default as Shield } from '@iconify-icons/ri/shield-line'
+export { default as Sword } from '@iconify-icons/ri/sword-line'
+export { default as Team } from '@iconify-icons/ri/team-fill'
+export { default as Analysis } from '@iconify-icons/ri/test-tube-fill'
+export { default as Missed } from '@iconify-icons/ri/windy-line'
+export { default as Coffee } from '@iconify-icons/simple-icons/buymeacoffee'
+// Support & compliance icons
+export { default as Discord } from '@iconify-icons/simple-icons/discord'
+export { default as GitHub } from '@iconify-icons/simple-icons/github'
+export { default as City } from '@iconify-icons/tabler/building-community' // Shrine
+export { default as Shrine } from '@iconify-icons/tabler/building-pavilion' // Shrine
+export { default as Stadium } from '@iconify-icons/tabler/building-stadium' // League
+export { default as Tunnel } from '@iconify-icons/tabler/building-tunnel' // Tunnel / Cave
+export { default as Download } from '@iconify-icons/tabler/download'
 // UI Icons - Route types
 // Forest,Town/City,Ruins/Tunnel,Mountain,Route
 export { default as Grave } from '@iconify-icons/tabler/grave-2'
-
-export { default as Ruins } from '@iconify-icons/tabler/tower' // Castle / Ruins
-export { default as Stadium } from '@iconify-icons/tabler/building-stadium' // League
-export { default as Tunnel } from '@iconify-icons/tabler/building-tunnel' // Tunnel / Cave
-export { default as Shrine } from '@iconify-icons/tabler/building-pavilion' // Shrine
-export { default as City } from '@iconify-icons/tabler/building-community' // Shrine
 export { default as Town } from '@iconify-icons/tabler/home' // Shrine
-
-export { default as Forest } from '@iconify-icons/tabler/trees' // Forest
 export { default as Mountain } from '@iconify-icons/tabler/mountain' // Mt. / Mountain
-export { default as Route } from '@iconify-icons/tabler/sign-left' // Route
-export { default as Trophy } from '@iconify-icons/tabler/trophy' // Route
-
-
-export { default as Game } from '@iconify-icons/gg/games';
-export { default as Hand } from '@iconify-icons/fa-solid/hand-holding';
-export { default as Heart } from '@iconify-icons/ph/heart-straight-fill';
-export { default as Asterisk } from '@iconify-icons/ri/asterisk'; // Previously info
-export { default as Map } from '@iconify-icons/ph/map-trifold';
-export { default as Minus } from '@iconify-icons/gg/math-minus';
-export { default as Plus } from '@iconify-icons/gg/math-plus';
-
-export { default as Pip } from '@iconify-icons/ph/circle-fill';
-export { default as Track } from '@iconify-icons/ri/footprint-fill';
-export { default as File } from '@iconify-icons/gg/file-add';
-
-export { default as Check } from '@iconify-icons/ph/circle-wavy-check-fill';
-export { default as Transfer } from '@iconify-icons/gg/arrows-exchange';
-
-// Type & Stat Icons
-export { default as Fire } from '@iconify-icons/ri/fire-fill';
-export { default as Grass } from '@iconify-icons/ri/leaf-fill';
-export { default as Water } from '@iconify-icons/ph/drop-fill';
-
-export { default as Physical } from '@iconify-icons/fa-solid/fist-raised';
-export { default as Atk } from '@iconify-icons/fa-solid/fist-raised';
-export { default as Special } from '@iconify-icons/ph/target';
-
-export { default as HP } from '@iconify-icons/ph/heart-straight-fill';
-export { default as Spe } from '@iconify-icons/ph/lightning-fill';
-export { default as Spd } from '@iconify-icons/fa-solid/brain';
-export { default as Spa } from '@iconify-icons/fe/target';
-export { default as Def } from '@iconify-icons/ri/shield-fill';
-
-export { default as Shield } from '@iconify-icons/ri/shield-line';
-export { default as Sword } from '@iconify-icons/ri/sword-line';
-
-export { default as Shiny } from '@iconify-icons/wi/stars';
-export { default as Status } from '@iconify-icons/ph/yin-yang-fill';
-
-export { default as BarChart } from '@iconify-icons/ri/bar-chart-2-fill'
-export { default as Team } from '@iconify-icons/ri/team-fill'
-
 // Weather effects & Battle status
 export { default as Double } from '@iconify-icons/tabler/multiplier-2x'
-
-// Light & Dark mode
-export { default as DarkMode } from '@iconify-icons/ph/moon-fill';
-export { default as LightMode } from '@iconify-icons/ph/sun-fill';
-
-export { default as Desktop } from '@iconify-icons/ph/desktop';
-export { default as Mobile } from '@iconify-icons/ph/device-mobile';
-
-// Support & compliance icons
-export { default as Discord } from '@iconify-icons/simple-icons/discord';
-export { default as Coffee } from '@iconify-icons/simple-icons/buymeacoffee';
-export { default as Cookie } from '@iconify-icons/ph/cookie-fill';
-export { default as GitHub } from '@iconify-icons/simple-icons/github';
+export { default as Route } from '@iconify-icons/tabler/sign-left' // Route
+export { default as Ruins } from '@iconify-icons/tabler/tower' // Castle / Ruins
+export { default as Forest } from '@iconify-icons/tabler/trees' // Forest
+export { default as Trophy } from '@iconify-icons/tabler/trophy' // Route
+export { default as Shiny } from '@iconify-icons/wi/stars'
+export * as Icon from '@iconify/svelte/dist/OfflineIcon.svelte'
 
 // ---- TODO:
