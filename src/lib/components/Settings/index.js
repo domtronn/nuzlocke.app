952f97a98c6f3298e51cbfa56e5ac173ea6ecd9d
Dom Charlesworth <dgc336@gmail.com>
2023-05-01 09:51:45 +0100

    #112 Download & import save files (#121)
    * [feature] Add download button to the save component
    
    * [feature] Add upload button to the import modal

--- src/lib/components/Settings/index.js
+++ src/lib/components/Settings/index.js
@@ -1,3 +1,4 @@
-export { default as Settings } from './ModalController.svelte'
 export { default as Modal } from './Modal.svelte'
+export { default as Settings } from './ModalController.svelte'
 export { default as Wrapper } from './Wrapper.svelte'
+export { settingsDefault } from './_data.js'
--- src/lib/components/save.svelte
+++ src/lib/components/save.svelte
@@ -1,106 +1,183 @@
 <script>
-  export let id, created, updated = -1, name, game, attempts = -1
+  import { settingsDefault } from '$c/Settings'
 
-  import { onMount, getContext } from 'svelte'
-  import { activeGame, deleteGame, getGame, read, summarise } from '$lib/store'
+  export let id,
+    created,
+    updated = -1,
+    name,
+    game,
+    attempts = -1,
+    settings = settingsDefault
+
   import { fade } from 'svelte/transition'
+  import { onMount, getContext } from 'svelte'
+  import {
+    activeGame,
+    deleteGame,
+    getGame,
+    read,
+    readdata,
+    summarise,
+    IDS
+  } from '$lib/store'
   import day from '$lib/utils/date'
 
   import { NuzlockeStates } from '$lib/data/states'
-  import { PIcon, IconButton, Logo as Picture } from '$lib/components/core'
-
-  import Icon from '@iconify/svelte/dist/OfflineIcon.svelte'
-  import { Bin } from '$icons'
-  import { Share } from '$icons'
+  import { Bin, Download, Share } from '$icons'
+  import { Icon, PIcon, IconButton, Logo as Picture } from '$c/core'
 
   let ShareModal
-  onMount(_ => {
-    import('$lib/components/qr/ShareModal.svelte').then(mod => ShareModal = mod.default)
+  onMount((_) => {
+    import('$lib/components/qr/ShareModal.svelte').then(
+      (mod) => (ShareModal = mod.default)
+    )
   })
 
   let team, available, deceased
-  getGame(id).subscribe(read(summarise(data => {
-    team = data.team || []
-    available = data.available || []
-    deceased = data.deceased || []
-  })))
+  getGame(id).subscribe(
+    read(
+      summarise((data) => {
+        team = data.team || []
+        available = data.available || []
+        deceased = data.deceased || []
+      })
+    )
+  )
 
-  const ondelete = _ => deleteGame(id)
-  const onclick = _ => {
+  const ondelete = (_) => deleteGame(id)
+  const onclick = (_) => {
     activeGame.set(id)
     window.location = '/game'
   }
 
+  let downloadAnchor
+  const ondownload = (_) => {
+    const gameData = window.localStorage.getItem(IDS.game(id))
+    const meta = {
+      name,
+      attempts,
+      game,
+      id,
+      settings,
+      created,
+      updated
+    }
+
+    const data =
+      `text/json;charset=utf-8,` +
+      encodeURIComponent(
+        JSON.stringify({ __meta: meta, ...JSON.parse(gameData || '{}') })
+      )
+    downloadAnchor.setAttribute('href', 'data:' + data)
+    downloadAnchor.setAttribute('download', `Nuzlocke Tracker - ${name}.nzsav`)
+  }
+
   const { open } = getContext('simple-modal')
-  const onshare = _ => open(ShareModal, { id })
+  const onshare = (_) => open(ShareModal, { id })
 
   $: date = day(+created).format('Do of MMM, YYYY')
   $: last = updated > created ? day(+updated).format('Do of MMM, YYYY') : null
 </script>
 
-<div class='transition tracking-widest flex flex-row justify-between items-center'>
-  <button title='Load saved game {name}'
-          class='group flex flex-col flex-1 sm:flex-row gap-x-4 flex-start items-start md:items-center cursor-pointer md:pr-24'
-          out:fade on:click={onclick}>
+<div
+  class="flex flex-row items-center justify-between tracking-widest transition"
+>
+  <button
+    title="Load saved game {name}"
+    class="flex-start group flex flex-1 cursor-pointer flex-col items-start gap-x-4 sm:flex-row md:items-center md:pr-24"
+    out:fade
+    on:click={onclick}
+  >
     <Picture
-      alt='{name} logo'
-      src=/assets/{game}
-      class='hidden sm:block h-16 w-32'
-      aspect=192x96
+      alt="{name} logo"
+      src="/assets/{game}"
+      class="hidden h-16 w-32 sm:block"
+      aspect="192x96"
     />
 
-    <div class='text-left'>
-      <h2 class='font-bold transition text-xl leading-7 max-w-[26ch]'>
-        <mark class='bg-transparent dark:text-gray-50 dark:group-hover:text-gray-900 transition group-hover:bg-yellow-300'>
+    <div class="text-left">
+      <h2 class="max-w-[26ch] text-xl font-bold leading-7 transition">
+        <mark
+          class="bg-transparent transition group-hover:bg-yellow-300 dark:text-gray-50 dark:group-hover:text-gray-900"
+        >
           {name}
           {#if attempts > 1}
-            <div class='opacity-50 italic font-normal text-xs -translate-y-0.5 inline-block'>
+            <div
+              class="inline-block -translate-y-0.5 text-xs font-normal italic opacity-50"
+            >
               (Attempt {attempts})
             </div>
-            {/if}
+          {/if}
         </mark>
       </h2>
 
-      <h3 class='text-sm transition'>
-        <mark class='bg-transparent dark:text-gray-50 dark:group-hover:text-gray-900 transition group-hover:bg-yellow-300'>{date}</mark>
+      <h3 class="text-sm transition">
+        <mark
+          class="bg-transparent transition group-hover:bg-yellow-300 dark:text-gray-50 dark:group-hover:text-gray-900"
+          >{date}</mark
+        >
         {#if last}
           <span> - </span>
-          <mark class='bg-transparent dark:text-gray-50 dark:group-hover:text-gray-900 transition group-hover:bg-yellow-300'>{last}</mark>
+          <mark
+            class="bg-transparent transition group-hover:bg-yellow-300 dark:text-gray-50 dark:group-hover:text-gray-900"
+            >{last}</mark
+          >
         {/if}
       </h3>
 
-      <span class='transition group-hover:grayscale-0 grayscale font-sans inline-flex items-center'>
-
-        <PIcon className=' -mt-0.5 -ml-1' type='item' name='poke-ball' />
+      <span
+        class="inline-flex items-center font-sans grayscale transition group-hover:grayscale-0"
+      >
+        <PIcon className=" -mt-0.5 -ml-1" type="item" name="poke-ball" />
         {(available || []).length}
-        <Icon inline={true} class='ml-3 mr-2 fill-current' icon={NuzlockeStates[5].icon} />
+        <Icon
+          inline={true}
+          class="ml-3 mr-2 fill-current"
+          icon={NuzlockeStates[5].icon}
+        />
         {(deceased || []).length}
 
-        <span class='mr-4' />
+        <span class="mr-4" />
 
         {#each team as icon}
-          <div class='w-8 h-8 relative'>
-            <PIcon class='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' name={icon} />
+          <div class="relative h-8 w-8">
+            <PIcon
+              class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+              name={icon}
+            />
           </div>
         {/each}
       </span>
     </div>
   </button>
 
-  <div class='flex flex-col gap-y-2 md:flex-row md:gap-x-3 items-end md:items-center'>
+  <div
+    class="flex flex-col items-end gap-y-2 md:flex-row md:items-center md:gap-x-3"
+  >
     <IconButton
       rounded
-      color=yellow
+      color="yellow"
       src={Bin}
-      title='Delete save'
-      on:click={ondelete} />
+      title="Delete save"
+      on:click={ondelete}
+    />
+
+    <a bind:this={downloadAnchor}>
+      <IconButton
+        rounded
+        color="yellow"
+        src={Download}
+        title="Download save"
+        on:click={ondownload}
+      />
+    </a>
 
     <IconButton
       rounded
-      color=yellow
+      color="yellow"
       src={Share}
-      title='Transfer save'
-      on:click={onshare} />
-
+      title="Transfer save"
+      on:click={onshare}
+    />
   </div>
 </div>
